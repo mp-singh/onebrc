a91ad1e7255474c960de9415c63919039664de78
Prince <mandeep@mandeepsingh.ca>
2024-01-21 19:27:23 +0000

    another soln

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use onebrc::solns::soln2::soln2;
 
 fn main() {
     //fastest solution thus far.
+    // soln2a();
     soln2();
 }
--- src/solns/soln2.rs
+++ src/solns/soln2.rs
@@ -1,7 +1,9 @@
 use std::{
+    char,
     collections::HashMap,
     fs::File,
-    io::{BufRead, BufReader},
+    io::{self, BufRead, BufReader, Cursor, Read, Seek},
+    os::unix::process,
 };
 
 use itertools::Itertools;
@@ -45,4 +47,76 @@ pub fn soln2() {
     }
     println!("\nsoln2: {:?}", start.elapsed());
     // std::process::exit(0); // comment out this line to benchmark
+}
+
+pub fn soln2a() {
+    let start = std::time::Instant::now();
+    let file = File::open("measurements.txt").expect("Failed opening file");
+    let chunk_size = 1024 * 1024; // 1MB
+    let mut buffer = vec![0; chunk_size]; // 1KB buffer
+    let mut reader = BufReader::with_capacity(chunk_size * 8, file);
+    let mut records = HashMap::<String, Temperature>::with_capacity(10_000);
+    let mut offset = 0;
+    let mut count = 0;
+    loop {
+        reader
+            .seek(io::SeekFrom::Start(offset))
+            .expect("Failed to seek");
+        let bytes_read = reader.read(&mut buffer).expect("Failed to read file");
+        if bytes_read == 0 {
+            break;
+        }
+        let last_newline = buffer[..bytes_read].iter().rposition(|&x| x == b'\n');
+
+        if let Some(index) = last_newline {
+            process_chunk(&buffer[..=index], &mut records, &mut count);
+            offset = offset + index as u64 + 1;
+        }
+    }
+    println!("time after loop: {:?}", start.elapsed());
+    let mut keys = records.keys().collect::<Vec<_>>();
+    keys.par_sort_unstable();
+    for key in keys {
+        let _t = records.get(key).unwrap();
+        // println!("{}={:.1}/{:.1}/{:.1}", key, t.min, t.mean(), t.max);
+    }
+    println!("\nsoln2: {:?}", start.elapsed());
+}
+
+fn process_chunk(chunk: &[u8], records: &mut HashMap<String, Temperature>, count: &mut usize) {
+    let chunk = std::str::from_utf8(chunk).unwrap();
+    for line in chunk.lines() {
+        *count += 1;
+        let (name, temp) = line.split_once(';').unwrap();
+        let temp = temp.parse::<f32>().unwrap();
+        if let Some(t) = records.get_mut(name) {
+            t.sum += temp;
+            t.count += 1;
+            if t.min > temp {
+                t.min = temp;
+                continue;
+            }
+            if t.max < temp {
+                t.max = temp;
+            }
+        } else {
+            records.insert(name.to_string(), Temperature::new(temp, temp, temp));
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // fn test_soln2a() {
+    //     soln2a();
+    // }
+    #[test]
+    fn test_soln2() {
+        soln2();
+    }
 }

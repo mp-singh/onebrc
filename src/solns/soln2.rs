a72157f78aa5e58bb65a98ce0d103b43110cc46b
Prince <mandeep@mandeepsingh.ca>
2024-01-22 05:45:37 +0000

    cleanup

--- src/solns/soln2.rs
+++ src/solns/soln2.rs
@@ -1,26 +1,51 @@
 use std::{
-    char,
     collections::HashMap,
     fs::File,
-    io::{self, BufRead, BufReader, Cursor, Read, Seek},
-    os::unix::process,
+    io::{BufReader, Read},
+    os::unix::fs::MetadataExt,
 };
 
-use itertools::Itertools;
 use rayon::slice::ParallelSliceMut;
 
 use crate::solns::Temperature;
 
 pub fn soln2() {
     let start = std::time::Instant::now();
-    let chunk_size = 1_000_000;
     let file = File::open("measurements.txt").expect("Failed opening file");
-    let lines = BufReader::new(file).lines().chunks(chunk_size);
+    let mmap = unsafe { memmap::Mmap::map(&file).expect("failed to map file") };
+    let file_size = file.metadata().unwrap().size();
+    let mut contents = String::with_capacity(file_size as usize);
+    let mut reader = BufReader::new(mmap.as_ref());
+    reader
+        .read_to_string(&mut contents)
+        .expect("Failed reading file");
 
+    let num_of_threads = 32;
+    let mut chunks = Vec::<String>::with_capacity(num_of_threads as usize);
+    let contents = contents.as_bytes().to_vec();
+
+    println!("gathering chunks of size {}", file_size / num_of_threads);
+    for i in 0..num_of_threads {
+        work(file_size, num_of_threads, i, &contents, &mut chunks);
+    }
+
+    // let remaining = contents.len() - chunks.iter().map(|c| c.len()).sum::<usize>();
+    // if remaining < (file_size / num_of_threads) as usize {
+    //     let chunk = &contents[contents.len() - remaining..];
+    //     chunks.push(chunk.iter().map(|c| char::from(*c)).collect::<String>());
+    // }
+    println!("finished gathering chunks in {:?}", start.elapsed());
+
+    let start1 = std::time::Instant::now();
     let mut records = HashMap::<String, Temperature>::with_capacity(10_000);
-    lines.into_iter().for_each(|chunks| {
-        for line in chunks {
-            let line = line.unwrap();
+    // create an atomic counter
+    let count = std::sync::atomic::AtomicUsize::new(0);
+    chunks.iter().for_each(|chunks| {
+        for line in chunks.lines() {
+            count.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            if line.is_empty() {
+                return;
+            }
             let (name, temp) = line.split_once(';').unwrap();
             let temp = temp.parse::<f32>().unwrap();
             if let Some(t) = records.get_mut(name) {
@@ -28,7 +53,7 @@ pub fn soln2() {
                 t.count += 1;
                 if t.min > temp {
                     t.min = temp;
-                    return;
+                    continue;
                 }
                 if t.max < temp {
                     t.max = temp;
@@ -38,85 +63,38 @@ pub fn soln2() {
             }
         }
     });
-
+    println!("finished processing in {:?}", start1.elapsed());
     let mut keys = records.keys().collect::<Vec<_>>();
     keys.par_sort_unstable();
     for key in keys {
         let t = records.get(key).unwrap();
         println!("{}={:.1}/{:.1}/{:.1}", key, t.min, t.mean(), t.max);
     }
-    println!("\nsoln2: {:?}", start.elapsed());
-    // std::process::exit(0); // comment out this line to benchmark
+    println!("Time taken: {:?}, \ncount: {:?}", start.elapsed(), count);
 }
 
-pub fn soln2a() {
-    let start = std::time::Instant::now();
-    let file = File::open("measurements.txt").expect("Failed opening file");
-    let chunk_size = 1024 * 1024; // 1MB
-    let mut buffer = vec![0; chunk_size]; // 1KB buffer
-    let mut reader = BufReader::with_capacity(chunk_size * 8, file);
-    let mut records = HashMap::<String, Temperature>::with_capacity(10_000);
-    let mut offset = 0;
-    let mut count = 0;
-    loop {
-        reader
-            .seek(io::SeekFrom::Start(offset))
-            .expect("Failed to seek");
-        let bytes_read = reader.read(&mut buffer).expect("Failed to read file");
-        if bytes_read == 0 {
-            break;
+fn work(file_size: u64, num_of_threads: u64, i: u64, contents: &[u8], chunks: &mut Vec<String>) {
+    let mut from = file_size / num_of_threads * i;
+    let mut to = file_size / num_of_threads * (i + 1); // exclusive
+    if from != 0 {
+        while contents[from as usize] != b'\n' {
+            from -= 1;
         }
-        let last_newline = buffer[..bytes_read].iter().rposition(|&x| x == b'\n');
-
-        if let Some(index) = last_newline {
-            process_chunk(&buffer[..=index], &mut records, &mut count);
-            offset = offset + index as u64 + 1;
-        }
+        // exclude the newline
+        from += 1;
     }
-    println!("time after loop: {:?}", start.elapsed());
-    let mut keys = records.keys().collect::<Vec<_>>();
-    keys.par_sort_unstable();
-    for key in keys {
-        let _t = records.get(key).unwrap();
-        // println!("{}={:.1}/{:.1}/{:.1}", key, t.min, t.mean(), t.max);
+
+    while to < file_size && contents[to as usize] != b'\n' {
+        to -= 1;
     }
-    println!("\nsoln2: {:?}", start.elapsed());
-}
 
-fn process_chunk(chunk: &[u8], records: &mut HashMap<String, Temperature>, count: &mut usize) {
-    let chunk = std::str::from_utf8(chunk).unwrap();
-    for line in chunk.lines() {
-        *count += 1;
-        let (name, temp) = line.split_once(';').unwrap();
-        let temp = temp.parse::<f32>().unwrap();
-        if let Some(t) = records.get_mut(name) {
-            t.sum += temp;
-            t.count += 1;
-            if t.min > temp {
-                t.min = temp;
-                continue;
-            }
-            if t.max < temp {
-                t.max = temp;
-            }
-        } else {
-            records.insert(name.to_string(), Temperature::new(temp, temp, temp));
-        }
+    if to != file_size {
+        // include the newline
+        to += 1;
+    } else {
+        to = file_size;
     }
-}
 
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    // fn test_soln2a() {
-    //     soln2a();
-    // }
-    #[test]
-    fn test_soln2() {
-        soln2();
-    }
+    let chunk = &contents[from as usize..to as usize];
+    chunks.push(chunk.iter().map(|c| char::from(*c)).collect::<String>());
 }
